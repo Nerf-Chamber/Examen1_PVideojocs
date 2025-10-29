7019da3f5daf1dda1ed1fe82280d231cc893bb6e
Nerf-Chamber <lzpobletpobletos@gmail.com>
2025-10-29 03:52:17 +0100

    Everything working properly

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private Vector2 actualPosition;
 
+    private bool _hasBounced = false;
+
     public float _enemyLimitPositionXPositive = 10;
     public float _enemyLimitPositionXNegative = -10;
 
@@ -17,10 +19,15 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        if (_rb.position.x >= 10 || _rb.position.x <= -10)
+        if (!_hasBounced && (_rb.position.x >= 10 || _rb.position.x <= -10))
         {
             _rb.linearVelocityX *= -1;
-            _rb.transform.position = new Vector2(_rb.transform.position.x, _rb.transform.position.y - 0.02f);
+            _rb.transform.position = new Vector2(_rb.transform.position.x, _rb.transform.position.y - 1.5f);
+            _hasBounced = true;
+        }
+        if (_rb.position.x < 10 && _rb.position.x > -10)
+        {
+            _hasBounced = false;
         }
         if (_rb.position.y <= 0)
         {
